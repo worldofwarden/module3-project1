4c2733942a96230f344945776e7df9183d2287d7
danhja <danhja1992@gmail.com>
2024-03-25 11:19:08 +0100

    Initial Commit

---
+++ JSS_M3_L1_S-main/js/app.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".aboutMe", {
+    strings: ["John Doe", "The Cake Master","a Confectioner","a Man with Passion"],
+    smartBackspace: true,
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+    loopCount: Infinity,
+    startDelay: 1000
+});
